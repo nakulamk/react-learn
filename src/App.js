f50a86c7193b2155f0571438e8175101ebc090f8
Nakula Gowda <nakulamk@gmail.com>
2023-03-25 10:50:47 +0530

    get-request-firebase

--- src/App.js
+++ src/App.js
@@ -1,75 +1,59 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-// import Tasks from "./components/Tasks/Tasks";
-// import NewTask from "./components/NewTask/NewTask";
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
 
-// function App() {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [tasks, setTasks] = useState([]);
-
-//   const fetchTasks = async (taskText) => {
-//     setIsLoading(true);
-//     setError(null);
-//     try {
-//       const response = await fetch(
-//         "https://react-http-f53c8-default-rtdb.firebaseio.com/tasks.json"
-//       );
-
-//       if (!response.ok) {
-//         throw new Error("Request failed!");
-//       }
-
-//       const data = await response.json();
-
-//       const loadedTasks = [];
-
-//       for (const taskKey in data) {
-//         loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-//       }
-
-//       setTasks(loadedTasks);
-//     } catch (err) {
-//       setError(err.message || "Something went wrong!");
-//     }
-//     setIsLoading(false);
-//   };
-
-//   useEffect(() => {
-//     fetchTasks();
-//   }, []);
-
-//   const taskAddHandler = (task) => {
-//     setTasks((prevTasks) => prevTasks.concat(task));
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <NewTask onAddTask={taskAddHandler} />
-//       <Tasks
-//         items={tasks}
-//         loading={isLoading}
-//         error={error}
-//         onFetch={fetchTasks}
-//       />
-//     </React.Fragment>
-//   );
-// }
-
-// export default App;
-
-import React, { useState, useEffect } from "react";
-import NewTask from "./components/NewTask/NewTask";
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
-  console.log(tasks);
+
   return (
     <React.Fragment>
-      <NewTask onAddTask={taskAddHandler}></NewTask>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
 }
+
 export default App;
